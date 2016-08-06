76145e99ec627e23a9374cf3d169c847b0544a02
supakornbabe <supakornbabew@hotmail.com>
2016-08-06 16:03:58 +0700

    add digit

--- digit.cpp
+++ digit.cpp
@@ -9,23 +9,38 @@ int main()
     char adown[100];
     char aup[100];
     int numban;
-    char ban[9]
+    char ban[9];
 
     cin>>a;
     strcpy(aup,a);
     strcpy(adown,a);
     cin>>numban;
+    int lena=strlen(a);
+    for (int i=0; i<numban; i++)cin>>ban[i];
 
-    for (int i=0; i<n; i++)cin>>ban[i];
-
-    for (int i=0; i<strlen(a); i++)
+    for (int i=0; i<lena; i++)
     {
-        for (int j=0; j<n; j++)
+        for (int j=0; j<numban; j++)
         {
-            if(a[i] == ban[j])
+            if(adown[i] == ban[j])
             {
-                if(aup[i] == '0') {}
+                adown[i]-=1;
+                if(i+1<lena)adown[i+1]='9';
             }
         }
     }
+    for (int i=0; i<lena; i++)
+    {
+        for (int j=0; j<numban; j++)
+        {
+            if(aup[i] == ban[j])
+            {
+                aup[i]+=1;
+                if(i+1<lena)aup[i+1]='0';
+            }
+        }
+    }
+    if(adown[0]=='0')cout<<"impossible"<<endl;
+    else cout<<adown<<endl;
+    cout<<aup;
 }
