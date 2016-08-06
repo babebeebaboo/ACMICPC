6040e6b9b46c7afab2d4c503e7ba30ba091c09c7
supakornbabe <supakornbabew@hotmail.com>
2016-08-06 14:57:28 +0700

    add digit cpp

--- digit.c
+++ digit.c
@@ -1,1 +1,14 @@
+#include<stdio.h>
+#include<iostream>
 
+using namespace std;
+
+int main()
+{
+    char a[100];
+    int numban;
+    char ban[9]
+    cin>>a
+    scanf("%d",&numban);
+    for (i=0;i<n;i++)scanf("%c",&ban[i])
+}
---
+++ digit.cpp
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include<iostream>
+#include<string.h>
+using namespace std;
+
+int main()
+{
+    char a[100];
+    char adown[100];
+    char aup[100];
+    int numban;
+    char ban[9]
+
+    cin>>a;
+    strcpy(aup,a);
+    strcpy(adown,a);
+    cin>>numban;
+
+    for (int i=0; i<n; i++)cin>>ban[i];
+
+    for (int i=0; i<strlen(a); i++)
+    {
+        for (int j=0; j<n; j++)
+        {
+            if(a[i] == ban[j])
+            {
+                if(aup[i] == '0') {}
+            }
+        }
+    }
+}
